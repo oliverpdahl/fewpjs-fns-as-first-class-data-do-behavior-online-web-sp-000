ed0948382c2b7be8e5ecd6b7c702017232a2d367
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-08 18:05:38 +0000

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,16 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString){
+  const splitTimeString = timeString.slit(:);
+  const hour = parseInt(splitTimeString[0], 10);
+  // const minute = parseInt(splitTimeString[1], 10);
+  if (hour > 19) {
+    return 'Good Evening'
+  } else if (hour > 12) {
+    return 'Good Afternoon'
+  } else {
+    return 'Good Morning'
+  }
+}
 /* Write your implementation of displayMessage() */
